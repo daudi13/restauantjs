8a0fd839012915a8ed80544f42d749847fbae0d7
daudi13 <davouma13@gmail.com>
2023-03-13 09:16:44 +0300

    Add files

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Image from 'next/image'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
+    <div className={styles.container}>
       <Head>
         <title>Welcome | Home</title>
         <meta name="description" content="Pizza restauraunt near me" />
@@ -15,6 +16,6 @@ export default function Home() {
       <main className={styles.main}>
         <h1>Homepage</h1>
       </main>
-    </>
+    </div>
   )
 }
