34600fe013d3618d38d2ed8bfd4b303a5b3fe593
daudi13 <davouma13@gmail.com>
2023-03-13 09:32:28 +0300

    Add layout

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Image from 'next/image'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,9 +15,11 @@ export default function Home() {
         <meta name="description" content="Pizza restauraunt near me" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+      <Navbar/>
       <main className={styles.main}>
         <h1>Homepage</h1>
       </main>
+      <Footer/>
     </div>
   )
 }
