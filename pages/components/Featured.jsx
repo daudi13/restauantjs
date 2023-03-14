ec0ba08c33e01a4f78acbd25488c2bd0d5f7c748
daudi13 <davouma13@gmail.com>
2023-03-14 10:53:58 +0300

    Add chevron

--- pages/components/Featured.jsx
+++ pages/components/Featured.jsx
@@ -1,6 +1,15 @@
+import style from "@/styles/Featured.module.css"
+import Image from "next/image"
+import chevronLeft from "/public/img/arrowl.png"
+import chevronRight from "/public/img/arrowr.png"
+
+
 const Featured = () => {
   return (
-    <div>Featured</div>
+    <div className={StyleSheet.container}>
+      <Image src={chevronLeft} alt="chevron" width="50" height="50"/>
+      <Image src={chevronRight} alt="chevron" width="50" height="50"/>
+    </div>
   )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
+import Featured from './components/Featured'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,7 +14,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <main className={styles.main}>
-        <h1>Homepage</h1>
+        <Featured/>
       </main>
     </div>
   )
