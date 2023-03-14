8c546eb5cbbbe0bb79caa83e98add9d0a6a3cfe5
daudi13 <davouma13@gmail.com>
2023-03-14 05:50:32 +0300

    Add prev code

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -1,7 +1,13 @@
+import styles from "@/styles/Navbar.module.css"
+
 const Navbar = () => {
   return (
-    <div>
-      navbar
+    <div className={styles.container}>
+      <div className={styles.item}>
+      </div>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
+      <div className={styles.item}></div>
     </div>
   )
 }
