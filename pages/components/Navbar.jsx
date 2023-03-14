a21cfe42beee1fabba96adad11dcb71a88d2b168
daudi13 <davouma13@gmail.com>
2023-03-14 08:15:08 +0300

    Add additional navabar styling

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -9,9 +9,9 @@ const Navbar = () => {
         <div className={styles.callButton}>
           <Image src={pic} alt="telephone icon" width="32" height="32"/>
         </div>
-        <div className={styles.text}>
-          <div className={styles.texts}>ORDER NOW</div>
-          <div className={styles.texts}>+254-745-294-210</div>
+        <div className={styles.texts}>
+          <div className={styles.text}>+254-745-294-210</div>
+          <div className={styles.text}>ORDER NOW</div>
         </div>
       </div>
       <div className={styles.item}>center</div>
