c9b40182f4d0117f7c58e9729e7ae806ff1d2c0d
daudi13 <davouma13@gmail.com>
2023-03-14 08:26:39 +0300

    Add final styling

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -1,21 +1,38 @@
 import Image from "next/image"
 import styles from "@/styles/Navbar.module.css"
-import pic from "/public/img/telephone.png"
+import tel from "/public/img/telephone.png"
+import logo from "/public/img/logo.png"
+import cart from "/public/img/cart.png"
 
 const Navbar = () => {
   return (
     <div className={styles.container}>
       <div className={styles.item}>
         <div className={styles.callButton}>
-          <Image src={pic} alt="telephone icon" width="32" height="32"/>
+          <Image src={tel} alt="telephone icon" width="32" height="32"/>
         </div>
         <div className={styles.texts}>
           <div className={styles.text}>+254-745-294-210</div>
           <div className={styles.text}>ORDER NOW</div>
         </div>
       </div>
-      <div className={styles.item}>center</div>
-      <div className={styles.item}>right</div>
+      <div className={styles.item}>
+        <ul className={styles.list}>
+          <li className={styles.listItem}>Homepage</li>
+          <li className={styles.listItem}>Product</li>
+          <li className={styles.listItem}>Menu</li>
+          <Image src={logo} alt="logo icon" width="160" height="69"/>
+          <li className={styles.listItem}>Events</li>
+          <li className={styles.listItem}>Blog</li>
+          <li className={styles.listItem}>Contacts</li>
+        </ul>
+      </div>
+      <div className={styles.cartItem}>
+        <div className={styles.cart}>
+          <Image src={cart} alt="telephone icon" width="32" height="32"/>
+        </div>
+        <div class className={styles.counter}>2</div>
+      </div>
     </div>
   )
 }
