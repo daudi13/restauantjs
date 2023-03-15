912de0c4258fae8bccd8ef27fc54131e63126b2f
daudi13 <davouma13@gmail.com>
2023-03-15 08:43:02 +0300

    Add original images

--- pages/components/Featured.jsx
+++ pages/components/Featured.jsx
@@ -2,9 +2,9 @@ import style from "@/styles/Featured.module.css"
 import Image from "next/image"
 import chevronLeft from "/public/img/arrowl.png"
 import chevronRight from "/public/img/arrowr.png"
-import featured1 from "/public/img/featured1.png"
+import featured1 from "/public/img/featured.png"
 import featured2 from "/public/img/featured2.png"
-import featured3 from "/public/img/pizza.png"
+import featured3 from "/public/img/featured3.png"
 
 const pizzas = [
   featured1,
@@ -16,19 +16,19 @@ const pizzas = [
 const Featured = () => {
   return (
     <div className={style.container}>
-      <Image src={chevronLeft} alt="chevron" width="50" height="50" />
+      <Image src={chevronLeft} alt="chevron" layout="fill" />
       <div className={style.wrapper}>
-        <div  key={i}className={style.imgContainer}>
+        <div className={style.imgContainer}>
           {
             pizzas.map((pizza, i) => {
             return(
-              <Image src={pizza} alt="pizza" width="350" height="" />
+              <Image src={pizza} key={i} alt="pizza" width="350" height="" />
               )
             })
           }
         </div>
       </div>
-      <Image src={chevronRight} alt="chevron" width="50" height="50"/>
+      <Image src={chevronRight} alt="chevron" layout="fill"/>
     </div>
   )
 }
