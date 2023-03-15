9be6e4af3595e1eb19ab3c7857f6fbb4c8ceec25
daudi13 <davouma13@gmail.com>
2023-03-15 07:26:35 +0300

    Add images

--- pages/components/Featured.jsx
+++ pages/components/Featured.jsx
@@ -2,6 +2,15 @@ import style from "@/styles/Featured.module.css"
 import Image from "next/image"
 import chevronLeft from "/public/img/arrowl.png"
 import chevronRight from "/public/img/arrowr.png"
+import featured1 from "/public/img/featured1.jpg"
+import featured2 from "/public/img/featured2.jpeg"
+import featured3 from "/public/img/featured3.jpeg"
+
+const pizzas = [
+  featured1,
+  featured2,
+  featured3,
+]
 
 
 const Featured = () => {
@@ -9,21 +18,15 @@ const Featured = () => {
     <div className={style.container}>
       <Image src={chevronLeft} alt="chevron" width="50" height="50" />
       <div className={style.wrapper}>
-        <div className={style.imgContainer}>
-          <Image src="" alt="pizza" width="150" height="150" />
-        </div>
-        <div className={style.imgContainer}>
-          <Image src="" alt="pizza" width="150" height="150" />
-        </div>
-        <div className={style.imgContainer}>
-          <Image src="" alt="pizza" width="150" height="150" />
-        </div>
-        <div className={style.imgContainer}>
-          <Image src="" alt="pizza" width="150" height="150" />
-        </div>
-        <div className={style.imgContainer}>
-          <Image src="" alt="pizza" width="150" height="150" />
-        </div>
+        {
+          pizzas.map((pizza, i) => {
+            return(
+              <div  key={i}className={style.imgContainer}>
+                <Image src={pizza} alt="pizza" width="150" height="150" />
+              </div>
+            )
+          })
+        }
       </div>
       <Image src={chevronRight} alt="chevron" width="50" height="50"/>
     </div>
