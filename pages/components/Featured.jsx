7be8ea5a57be8df0f93165c97c97a181dc1df8ae
daudi13 <davouma13@gmail.com>
2023-03-15 07:19:09 +0300

    Add pizza images

--- pages/components/Featured.jsx
+++ pages/components/Featured.jsx
@@ -6,8 +6,25 @@ import chevronRight from "/public/img/arrowr.png"
 
 const Featured = () => {
   return (
-    <div className={StyleSheet.container}>
-      <Image src={chevronLeft} alt="chevron" width="50" height="50"/>
+    <div className={style.container}>
+      <Image src={chevronLeft} alt="chevron" width="50" height="50" />
+      <div className={style.wrapper}>
+        <div className={style.imgContainer}>
+          <Image src="" alt="pizza" width="150" height="150" />
+        </div>
+        <div className={style.imgContainer}>
+          <Image src="" alt="pizza" width="150" height="150" />
+        </div>
+        <div className={style.imgContainer}>
+          <Image src="" alt="pizza" width="150" height="150" />
+        </div>
+        <div className={style.imgContainer}>
+          <Image src="" alt="pizza" width="150" height="150" />
+        </div>
+        <div className={style.imgContainer}>
+          <Image src="" alt="pizza" width="150" height="150" />
+        </div>
+      </div>
       <Image src={chevronRight} alt="chevron" width="50" height="50"/>
     </div>
   )
