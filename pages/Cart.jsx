049f7e41ea1af8f749e7fde7c951b76fcc5e1e32
daudi13 <davouma13@gmail.com>
2023-03-20 06:14:35 +0300

    Add cart component and cart styling

---
+++ pages/Cart.jsx
@@ -0,0 +1,12 @@
+import styles from "@/styles/Cart.modules.css";
+import Image from "next/image";
+
+const Cart = () => {
+  return (
+    <div className={styles.container}>
+      
+    </div>
+  )
+}
+
+export default Cart
