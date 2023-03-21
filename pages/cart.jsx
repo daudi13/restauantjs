666b6fed53edf51694b7c1e2d5a838fd5f9cde28
daudi13 <davouma13@gmail.com>
2023-03-21 07:45:09 +0300

    Add order page

---
+++ pages/cart.jsx
@@ -0,0 +1,63 @@
+import styles from "@/styles/Cart.module.css";
+import Image from "next/image";
+import pizza from "/public/img/pizza.png"
+
+const Cart = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <table className={styles.table}>
+          <tr className={styles.tr}>
+            <th>Product</th>
+            <th>Name</th>
+            <th>Extras</th>
+            <th>Price</th>
+            <th>Quantity</th>
+            <th>Total</th>
+          </tr>
+          <tr>
+            <td className={styles.box1}>
+              <div className={styles.imgContainer}>
+                <Image src={pizza} objectFit="cover" alt="" width="100" height="100" />
+              </div>
+            </td>
+            <td className={styles.box}>
+              <div className={styles.name}>CORALZO</div>
+            </td>
+            <td className={styles.box}>
+              <div className={styles.extras}>
+                Double Ingredients, spicy sauce
+              </div>
+            </td>
+            <td className={styles.box}>
+              <div className={styles.price}>$19.90</div>
+            </td>
+            <td className={styles.box}>
+              <div className={styles.quantity}>2</div>
+            </td>
+            <td className={styles.box}>
+              <div className={styles.total}>$39.80</div>
+            </td>
+          </tr>
+        </table>
+      </div>
+      <div className={styles.right}>
+        <div className={styles.wrapper}>
+          <h2 className={styles.title}>CART TOTAL</h2>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Subtotal:</b>$79.60
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Discount:</b>$0.00
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Total:</b>$79.60
+          </div>
+          <button className={styles.button}>Check out</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Cart
---
+++ pages/order/[id].jsx
@@ -0,0 +1,13 @@
+import style from "@/styles/Order.module.css"
+import Image from "next/image"
+
+const Order = () => {
+  return (
+    <div className={style.container}>
+      <div className={style.left}></div> 
+      <div className={style.right}></div> 
+    </div>
+  )
+}
+
+export default Order
