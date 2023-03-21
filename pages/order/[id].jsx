e5ad4324dba6e3757c57a4c58098539b18fd5457
daudi13 <davouma13@gmail.com>
2023-03-21 07:54:38 +0300

    Add order page styling

--- pages/order/[id].jsx
+++ pages/order/[id].jsx
@@ -1,11 +1,25 @@
-import style from "@/styles/Order.module.css"
+import styles from "@/styles/Order.module.css"
 import Image from "next/image"
 
 const Order = () => {
   return (
-    <div className={style.container}>
-      <div className={style.left}></div> 
-      <div className={style.right}></div> 
+    <div className={styles.container}>
+      <div className={styles.left}></div> 
+      <div className={styles.right}>
+        <div className={styles.wrapper}>
+          <h2 className={styles.title}>CART TOTAL</h2>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Subtotal:</b>$79.60
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Discount:</b>$0.00
+          </div>
+          <div className={styles.totalText}>
+            <b className={styles.totalTextTitle}>Total:</b>$79.60
+          </div>
+          <button className={styles.button} disabled>PAID</button>
+        </div>
+      </div> 
     </div>
   )
 }
