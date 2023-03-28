e2f7cf4e0a5938e09b9169bc511f44282559ce6e
daudi13 <davouma13@gmail.com>
2023-03-28 08:04:25 +0300

    Add updates

--- pages/components/PizzaCards.jsx
+++ pages/components/PizzaCards.jsx
@@ -1,15 +1,15 @@
 import styles from "@/styles/PizzaCards.module.css"
 import Image from "next/image"
-import pizza from "/public/img/pizza.png"
 
-const PizzaCards = () => {
+const PizzaCards = ({ pizza }) => {
+  const Img = pizza.image
   return (
     <div className={styles.container}>
-      <Image src={pizza} alt="pizza" width="200" height="200" />
-      <h1 className={styles.title}>FIORI DI ZUCCA</h1>
-      <span className={styles.price}>$19.90</span>
+      <Image src={Img} alt="pizza" width="200" height="200" />
+      <h1 className={styles.title}>{pizza.title}</h1>
+      <span className={styles.price}>{pizza.price}</span>
       <p className={styles.desc}>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora suscipit nam perferendis nulla nisi, sunt, repellendus temporibus perspiciatis pariatur placeat doloribus. Recusandae, dolor blanditiis! Dolor natus dolorem est mollitia voluptate!
+        {pizza.desc}
       </p>
     </div>
   )
--- pages/index.js
+++ pages/index.js
@@ -3,10 +3,12 @@ import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import Featured from './components/Featured'
 import PizzaList from './components/PizzaList'
+import axios from 'axios'
 
 const inter = Inter({ subsets: ['latin'] })
 
-export default function Home() {
+export default function Home({ pizzaList }) {
+  console.log(pizzaList)
   return (
     <div className={styles.container}>
       <Head>
@@ -15,7 +17,16 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Featured />
-      <PizzaList/>
+      <PizzaList pizzaList={pizzaList} />
     </div>
   )
+}
+
+export const getServerSideProps = async () => {
+  const res = await axios("http://localhost:3000/api/products")
+  return {
+    props: {
+      pizzaList: res.data,
+    }
+  }
 }
