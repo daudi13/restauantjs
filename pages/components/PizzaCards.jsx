81eb6af428d1bf4a512e469a175598bb443664cf
daudi13 <davouma13@gmail.com>
2023-03-28 08:30:00 +0300

    Add prices

--- pages/components/PizzaCards.jsx
+++ pages/components/PizzaCards.jsx
@@ -5,7 +5,7 @@ const PizzaCards = ({ pizza }) => {
   const Img = pizza.image
   return (
     <div className={styles.container}>
-      <Image src={Img} alt="pizza" width="200" height="200" />
+      <Image src={`${Img}`} alt="pizza" width="200" height="200" />
       <h1 className={styles.title}>{pizza.title}</h1>
       <span className={styles.price}>{pizza.price}</span>
       <p className={styles.desc}>
