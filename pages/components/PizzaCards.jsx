297b1ec19be5b8f2ee94ff4151e35691b27d03dd
daudi13 <davouma13@gmail.com>
2023-03-28 08:42:54 +0300

    Add link center

--- pages/components/PizzaCards.jsx
+++ pages/components/PizzaCards.jsx
@@ -1,17 +1,20 @@
 import styles from "@/styles/PizzaCards.module.css"
 import Image from "next/image"
+import Link from "next/link"
 
 const PizzaCards = ({ pizza }) => {
   const Img = pizza.image
   return (
     <div className={styles.container}>
-      <Image src={`${Img}`} alt="pizza" width="200" height="200" />
-      <h1 className={styles.title}>{pizza.title}</h1>
-      <span className={styles.price}>${pizza.prices[0]}</span>
-      <p className={styles.desc}>
-        {pizza.desc}
-      </p>
-    </div>
+        <Link href={`/product/${pizza._id}`} className={styles.link}>
+          <Image src={`${Img}`} alt="pizza" width="200" height="200" />
+          <h1 className={styles.title}>{pizza.title}</h1>
+          <span className={styles.price}>${pizza.prices[0]}</span>
+          <p className={styles.desc}>
+            {pizza.desc}
+          </p>
+        </Link>
+      </div>
   )
 }
 
