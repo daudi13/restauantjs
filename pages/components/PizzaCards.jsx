918566de7c02a7b6b54b772ce8d4ea0699aa153c
daudi13 <davouma13@gmail.com>
2023-03-28 08:30:18 +0300

    Add dollar sign

--- pages/components/PizzaCards.jsx
+++ pages/components/PizzaCards.jsx
@@ -7,7 +7,7 @@ const PizzaCards = ({ pizza }) => {
     <div className={styles.container}>
       <Image src={`${Img}`} alt="pizza" width="200" height="200" />
       <h1 className={styles.title}>{pizza.title}</h1>
-      <span className={styles.price}>{pizza.price}</span>
+      <span className={styles.price}>${pizza.prices[0]}</span>
       <p className={styles.desc}>
         {pizza.desc}
       </p>
