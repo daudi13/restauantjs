c5dea8c2edd84aceacc1b515ddc165214c48ea3d
daudi13 <davouma13@gmail.com>
2023-04-04 07:48:32 +0300

    Add quanity

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -3,8 +3,11 @@ import styles from "@/styles/Navbar.module.css"
 import tel from "/public/img/telephone.png"
 import logo from "/public/img/logo.png"
 import cart from "/public/img/cart.png"
+import { useSelector } from "react-redux"
 
 const Navbar = () => {
+  const quantity = useSelector((state) => state.cart.quantity)
+
   return (
     <div className={styles.container}>
       <div className={styles.item}>
@@ -30,7 +33,7 @@ const Navbar = () => {
       <div className={styles.cartItem}>
         <div className={styles.cart}>
           <Image src={cart} alt="telephone icon" width="32" height="32"/>
-        <div className={styles.counter}>2</div>
+          <div className={styles.counter}>{quantity}</div>
         </div>
       </div>
     </div>
